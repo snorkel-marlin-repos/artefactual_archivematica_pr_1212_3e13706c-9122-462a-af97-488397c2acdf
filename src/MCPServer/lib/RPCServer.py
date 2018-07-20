d272dc1a4668f9423ae7fef7541c255be7801242
Payten Giles <payten.giles@gmail.com>
2018-07-20 10:02:20 +1000

    Capture any exception in index_aip to job's stderr

---
+++ src/MCPServer/lib/RPCServer.py
@@ -0,0 +1,139 @@
+"""Archivematica MCPServer RPC."""
+
+# This file is part of Archivematica.
+#
+# Copyright 2010-2018 Artefactual Systems Inc. <http://artefactual.com>
+#
+# Archivematica is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Archivematica is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
+
+import cPickle
+import logging
+import lxml.etree as etree
+from socket import gethostname
+import time
+
+from django.conf import settings as django_settings
+import gearman
+
+from linkTaskManagerChoice import choicesAvailableForUnits
+from package import create_package
+
+
+LOGGER = logging.getLogger("archivematica.mcp.server.rpcserver")
+
+
+class RPCServerError(Exception):
+    """Base exception of RPCServer."""
+
+
+class UnexpectedPayloadError(RPCServerError):
+    """Missing parameters in payload."""
+
+
+def log_exceptions(logger):
+    """Worker handler decorator to log exceptions."""
+    def decorator(func):
+        def wrap(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as err:
+                internal_err = isinstance(err, RPCServerError)
+                logger.error('Exception raised by handler %s: %s',
+                             func.__name__, err, exc_info=not internal_err)
+                raise  # So GearmanWorker knows that it failed.
+        return wrap
+    return decorator
+
+
+def unpickle_payload(fn):
+    """Worker handler decorator to unpickle the incoming payload.
+
+    It prepends an argument before calling the decorated function.
+    """
+    def wrap(*args, **kwargs):
+        gearman_job = args[1]
+        payload = cPickle.loads(gearman_job.data)
+        if not isinstance(payload, dict):
+            raise UnexpectedPayloadError('Payload is not a dictionary')
+        kwargs['payload'] = payload
+        return fn(*args, **kwargs)
+    return wrap
+
+
+def pickle_result(fn):
+    """Worker handler decorator to pickle the returned value."""
+    def wrap(*args, **kwargs):
+        return cPickle.dumps(fn(*args, **kwargs))
+    return wrap
+
+
+@pickle_result
+@unpickle_payload
+@log_exceptions(LOGGER)
+def job_approve_handler(*args, **kwargs):
+    payload = kwargs['payload']
+    job_id = payload["jobUUID"]
+    chain = payload["chain"]
+    user_id = str(payload["uid"])
+    LOGGER.debug("Approving: %s %s %s", job_id, chain, user_id)
+    if job_id in choicesAvailableForUnits:
+        choicesAvailableForUnits[job_id].proceedWithChoice(chain, user_id)
+    return "approving: ", job_id, chain
+
+
+@pickle_result
+@log_exceptions(LOGGER)
+def job_awaiting_approval_handler(*args):
+    ret = etree.Element('choicesAvailableForUnits')
+    for uuid, choice in choicesAvailableForUnits.items():
+        ret.append(choice.xmlify())
+    return etree.tostring(ret, pretty_print=True)
+
+
+@pickle_result
+@unpickle_payload
+@log_exceptions(LOGGER)
+def package_create_handler(*args, **kwargs):
+    """Handle create package request."""
+    payload = kwargs['payload']
+    create_package_args = (
+        payload.get('name'),
+        payload.get('type'),
+        payload.get('accession'),
+        payload.get('path'),
+        payload.get('metadata_set_id'),
+    )
+    return create_package(*create_package_args).pk
+
+
+def startRPCServer():
+    gm_worker = gearman.GearmanWorker([django_settings.GEARMAN_SERVER])
+    hostID = gethostname() + "_MCPServer"
+    gm_worker.set_client_id(hostID)
+
+    # The tasks registered in this worker should not block.
+    gm_worker.register_task("approveJob", job_approve_handler)
+    gm_worker.register_task("getJobsAwaitingApproval", job_awaiting_approval_handler)
+    gm_worker.register_task("packageCreate", package_create_handler)
+
+    failMaxSleep = 30
+    failSleep = 1
+    failSleepIncrementor = 2
+    while True:
+        try:
+            gm_worker.work()
+        except gearman.errors.ServerUnavailable:
+            time.sleep(failSleep)
+            if failSleep < failMaxSleep:
+                failSleep += failSleepIncrementor
---
+++ src/dashboard/src/contrib/mcp/client.py
@@ -0,0 +1,87 @@
+# This file is part of Archivematica.
+#
+# Copyright 2010-2013 Artefactual Systems Inc. <http://artefactual.com>
+#
+# Archivematica is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Archivematica is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Archivematica.  If not, see <http://www.gnu.org/licenses/>.
+
+import cPickle
+import logging
+
+from django.conf import settings
+import gearman
+
+
+LOGGER = logging.getLogger('archivematica.dashboard.mcp.client')
+
+
+class RPCError(Exception):
+    pass
+
+
+INFLIGHT_POLL_TIMEOUT = 5.0
+
+
+class MCPClient:
+    def __init__(self):
+        self.server = settings.GEARMAN_SERVER
+
+    def execute(self, uuid, choice, uid=None):
+        gm_client = gearman.GearmanClient([self.server])
+        data = {}
+        data["jobUUID"] = uuid
+        data["chain"] = choice
+        if uid is not None:
+            data["uid"] = uid
+        gm_client.submit_job("approveJob", cPickle.dumps(data), None)
+        gm_client.shutdown()
+        return
+
+    def list(self):
+        gm_client = gearman.GearmanClient([self.server])
+        completed_job_request = gm_client.submit_job("getJobsAwaitingApproval", "", None)
+        if completed_job_request.state == gearman.JOB_COMPLETE:
+            return cPickle.loads(completed_job_request.result)
+        elif completed_job_request.state == gearman.JOB_FAILED:
+            raise RPCError("getJobsAwaitingApproval failed (check MCPServer logs)")
+
+    def notifications(self):
+        gm_client = gearman.GearmanClient([self.server])
+        completed_job_request = gm_client.submit_job("getNotifications", "", None)
+        gm_client.shutdown()
+        if completed_job_request.state == gearman.JOB_COMPLETE:
+            return cPickle.loads(completed_job_request.result)
+        elif completed_job_request.state == gearman.JOB_FAILED:
+            raise RPCError("getNotifications failed (check MCPServer logs)")
+
+    def create_package(self, name, type_, accession, path, metadata_set_id,
+                       auto_approve=True, wait_until_complete=False):
+        gm_client = gearman.GearmanClient([self.server])
+        data = cPickle.dumps({
+            'name': name,
+            'type': type_,
+            'accession': accession,
+            'path': path,
+            'metadata_set_id': metadata_set_id,
+            'auto_approve': auto_approve,
+            'wait_until_complete': wait_until_complete,
+        })
+        response = gm_client.submit_job('packageCreate', data,
+                                        background=False,
+                                        wait_until_complete=True,
+                                        poll_timeout=INFLIGHT_POLL_TIMEOUT)
+        gm_client.shutdown()
+        if response.state == gearman.JOB_COMPLETE:
+            return cPickle.loads(response.result)  # Transfer ID (pickled)
+        elif response.state == gearman.JOB_FAILED:
+            raise RPCError('MCPServer returned an error (check the logs)')
